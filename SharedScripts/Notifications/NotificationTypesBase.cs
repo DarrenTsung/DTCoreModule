c5710f02d2c869daf3cf62810541b079e8f65adc
Darren Tsung <darrentsung@Darrens-MacBook-Pro.local>
2015-10-04 11:09:55 -0700

    Added switch editor script and notification module

---
+++ SharedScripts/Notifications/NotificationTypesBase.cs
@@ -0,0 +1,15 @@
+﻿using DT;
+using System.Collections;
+﻿using UnityEngine;
+
+public static class NotificationTypesBase {
+	// PRAGMA MARK - Player
+	// HandlePlayerChanged(GameObject player);
+	public const int PLAYER_CHANGED = 100; 
+	
+	// PRAGMA MARK - Input
+	// HandlePrimaryDirection(Vector2 primaryDirection);
+	public const int HANDLE_PRIMARY_DIRECTION = 200;
+	// HandleSecondaryDirection(Vector2 secondaryDirection);
+	public const int HANDLE_SECONDARY_DIRECTION = 201;
+}
