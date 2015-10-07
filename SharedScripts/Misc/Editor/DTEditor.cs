1805b5c21c9f549eba692401676d3d7a1b97c0fb
Darren Tsung <darrentsung@Darrens-MacBook-Pro.local>
2015-10-07 09:47:46 -0700

    Added listening to events to DTEditor

--- SharedScripts/Misc/Editor/DTEditor.cs
+++ SharedScripts/Misc/Editor/DTEditor.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 
 namespace DT {
 	public class DTEditor<T> : Editor where T : MonoBehaviour {
+		// PRAGMA MARK - INTERFACE
+		public override void OnInspectorGUI() {
+			if (GUI.changed) {
+				EditorUtility.SetDirty(_object);
+			}
+		}
+		
 		// PRAGMA MARK - INTERNAL
 		protected T _object;
 		
@@ -12,10 +19,21 @@ namespace DT {
       _object = target as T;
  		}
 		
-		public override void OnInspectorGUI() {
-			if (GUI.changed) {
-				EditorUtility.SetDirty(_object);
+		protected virtual void OnSceneGUI() {
+			this.ListenForEvents();
+		}
+		
+		protected void ListenForEvents() {
+			Event e = Event.current;
+			switch (e.type) {
+				case EventType.KeyDown:
+					this.HandleKeyDown(e.character);
+					break;
 			}
+		}
+		
+		protected virtual void HandleKeyDown(char character) {
+			// do nothing
 		}
 	}
 }
