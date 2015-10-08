3c8b8fc2357cf2ec0649168748b073e7c1ffc5cc
Darren Tsung <darrentsung@Darrens-MacBook-Pro.local>
2015-10-08 11:40:59 -0700

    Renamed to DTNotifications and added PlayerDied event

---
+++ SharedScripts/DTNotifications.cs
@@ -0,0 +1,19 @@
+﻿using DT;
+using System.Collections;
+﻿using UnityEngine;
+﻿using UnityEngine.Events;
+
+namespace DT {
+	public static class DTNotifications {
+		// NOTE (darren): notification snippet is useful here [notif, notifArgs]
+		
+		// PRAGMA MARK - Player
+		public static UnityEvents.G PlayerChanged = new UnityEvents.G();
+		public static UnityEvent PlayerDied = new UnityEvent();
+		
+		// PRAGMA MARK - Input
+		public static UnityEvents.V2 HandlePrimaryDirection = new UnityEvents.V2();
+		public static UnityEvents.V2 HandleSecondaryDirection = new UnityEvents.V2();
+		public static UnityEvents.V2 HandleMouseScreenPosition = new UnityEvents.V2();
+	}
+}
