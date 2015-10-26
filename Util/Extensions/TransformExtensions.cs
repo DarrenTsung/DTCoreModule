d2fa917a10f96d3d101ab9bf0fe0a4a4ca3542d6
Darren Tsung <darrentsung@Darrens-MacBook-Pro.local>
2015-10-26 10:07:53 -0700

    Destroy all children extension for transforms

--- Util/Extensions/TransformExtensions.cs
+++ Util/Extensions/TransformExtensions.cs
@@ -10,5 +10,22 @@ namespace DT {
   		}
   		return found;
   	}
+    
+    public static void DestroyAllChildren(this Transform transform, bool immediate = false) {
+      GameObject[] children = new GameObject[transform.childCount];
+      
+      int index = 0;
+      foreach (Transform child in transform) {
+        children[index++] = child.gameObject;
+      }
+      
+      for (int i = children.Length - 1; i >= 0; i--) {
+        if (immediate) {
+          GameObject.DestroyImmediate(children[i]);
+        } else {
+          GameObject.Destroy(children[i]);
+        }
+      }
+    }
   }
 }
