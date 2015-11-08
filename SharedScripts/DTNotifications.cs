21b8878c399fc645eac6e5756e08b8513f536b17
Darren Tsung <darrentsung@Darrens-MacBook-Pro.local>
2015-11-08 08:18:02 -0800

    Moved notifications that belonged to GameEngine from DTNotifications to DTGameEngineNotifications

--- SharedScripts/DTNotifications.cs
+++ SharedScripts/DTNotifications.cs
@@ -6,14 +6,5 @@ using System.Collections;
 namespace DT {
 	public static class DTNotifications {
 		// NOTE (darren): notification snippet is useful here [notif, notifArgs]
-		
-		// PRAGMA MARK - Player
-		public static UnityEvents.IG PlayerChanged = new UnityEvents.IG();
-		public static UnityEvents.I PlayerDied = new UnityEvents.I();
-		
-		// PRAGMA MARK - Input
-		public static UnityEvents.V2 HandleMouseScreenPosition = new UnityEvents.V2();
-		public static UnityEvents.IV2 HandlePrimaryDirection = new UnityEvents.IV2();
-		public static UnityEvents.IV2 HandleSecondaryDirection = new UnityEvents.IV2();
 	}
 }
