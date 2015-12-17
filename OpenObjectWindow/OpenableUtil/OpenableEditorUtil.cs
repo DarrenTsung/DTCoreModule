2e85e242626d059576fc21e83246bc610ce12505
Darren Tsung <darren.tsung@gmail.com>
2015-12-17 08:05:33 -0800

    Added a small DestroySelf component to shared scripts, added OpenableEditorUtil functions for Play and Quit

---
+++ OpenObjectWindow/OpenableUtil/OpenableEditorUtil.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEditor;
+
+namespace DT {
+  [OpenableClass]
+  public class OpenableEditorUtil {
+    [OpenableMethod]
+    public static void Quit() {
+      if (!EditorApplication.isPlaying) {
+        return;
+      }
+      
+      EditorApplication.isPlaying = false;
+    }
+    
+    [OpenableMethod]
+    public static void Play() {
+      if (EditorApplication.isPlaying) {
+        return;
+      }
+      
+      EditorApplication.isPlaying = true;
+    }
+  }
+}
