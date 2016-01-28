9107c694a0e2a92c432cc7c7228eeab495971929
Darren Tsung <darren.tsung@gmail.com>
2016-01-28 08:01:47 -0800

    Return IEnumerator instead of coroutine in DoAfterDelay

--- Extensions/MonobehaviourExtensions.cs
+++ Extensions/MonobehaviourExtensions.cs
@@ -8,39 +8,41 @@ namespace DT {
     public static T GetRequiredComponent<T>(this MonoBehaviour m) {
       return m.gameObject.GetRequiredComponent<T>();
     }
-    
+
     public static void SetSelfAsParent(this MonoBehaviour m, GameObject other) {
       other.transform.parent = m.transform;
     }
-    
+
     public static T GetCachedComponent<T>(this MonoBehaviour m, Dictionary<Type, MonoBehaviour> cache, bool searchChildren = false) where T : class {
       return m.gameObject.GetCachedComponent<T>(cache, searchChildren);
     }
-    
+
     public static T GetRequiredComponentInChildren<T>(this MonoBehaviour m) {
       return m.gameObject.GetRequiredComponentInChildren<T>();
     }
-    
+
     public static T GetOnlyComponentInChildren<T>(this MonoBehaviour m) {
       return m.gameObject.GetOnlyComponentInChildren<T>();
     }
-    
+
     public static T GetRequiredComponentInParent<T>(this MonoBehaviour m) {
       return m.gameObject.GetRequiredComponentInParent<T>();
     }
-    
-    public static Coroutine DoAfterDelay(this MonoBehaviour m, float delay, Action callback) {
+
+    public static IEnumerator DoAfterDelay(this MonoBehaviour m, float delay, Action callback) {
       if (delay < 0) {
         delay = 0;
       }
-      return m.StartCoroutine(m.DoActionAfterDelayCoroutine(delay, callback));
+      IEnumerator coroutine = m.DoActionAfterDelayCoroutine(delay, callback);
+      m.StartCoroutine(coroutine);
+      return coroutine;
     }
-    
+
     public static IEnumerator DoActionAfterDelayCoroutine(this MonoBehaviour m, float delay, Action callback) {
       yield return new WaitForSeconds(delay);
       callback();
     }
-    
+
     public static GameObject[] FindChildGameObjectsWithTag(this MonoBehaviour m, string tag) {
       return m.gameObject.FindChildGameObjectsWithTag(tag);
     }
