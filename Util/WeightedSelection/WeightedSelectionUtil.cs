c95d1f930c7f8ecbadce48744561c7bf6fce8c09
Darren Tsung <darren.tsung@gmail.com>
2016-02-17 10:17:10 -0800

    Add more validation to selectWeightedObject

--- Util/WeightedSelection/WeightedSelectionUtil.cs
+++ Util/WeightedSelection/WeightedSelectionUtil.cs
@@ -5,7 +5,16 @@ using UnityEngine;
 namespace DT {
   public static class WeightedSelectionUtil {
     public static T SelectWeightedObject<T>(IEnumerable<T> collection) where T : IWeightedObject {
+      if (collection == null) {
+        Debug.LogWarning("SelectWeightedObject - passed in null collection!");
+        return default(T);
+      }
+
       int cumulativeWeight = collection.Sum(obj => obj.Weight);
+      if (cumulativeWeight == 0) {
+        Debug.LogWarning("SelectWeightedObject - cumulative weight is 0!");
+        return default(T);
+      }
 
       int selectedWeight = Random.Range(0, cumulativeWeight);
       foreach (T obj in collection) {
