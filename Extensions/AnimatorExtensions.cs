3b4e7b10faf608ef995f70b3a812d82868fb2af9
Darren Tsung <darren.tsung@gmail.com>
2016-02-26 09:57:36 -0800

    Add Animator Extensions

---
+++ Extensions/AnimatorExtensions.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DT {
+  public static class AnimatorExtensions {
+    public static bool IsCurrentStateNamed(this Animator animator, int layerIndex, params string[] names) {
+      AnimatorStateInfo currentStateInfo = animator.GetCurrentAnimatorStateInfo(layerIndex);
+
+      foreach (string name in names) {
+        if (currentStateInfo.IsName(name)) {
+          return true;
+        }
+      }
+
+      return false;
+    }
+
+    public static T GetRequiredBehaviour<T>(this Animator animator) where T : StateMachineBehaviour {
+      T behaviour = animator.GetBehaviour<T>();
+      if (behaviour == null) {
+        Debug.LogError("GetRequiredBehaviour: Behaviour " + typeof(T).Name + " missing in " + animator.gameObject.FullName());
+      }
+      return behaviour;
+    }
+  }
+}
