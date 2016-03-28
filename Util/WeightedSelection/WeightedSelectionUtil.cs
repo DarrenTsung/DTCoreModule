f29da1664241246ed31acdc63446f02df73a7035
Darren Tsung <darren.tsung@gmail.com>
2016-03-28 09:10:28 -0700

    Switch WeightedSelectionUtil to take in a weightTransformation function to determine an object's weight

--- Util/WeightedSelection/WeightedSelectionUtil.cs
+++ Util/WeightedSelection/WeightedSelectionUtil.cs
@@ -1,24 +1,25 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
 namespace DT {
   public static class WeightedSelectionUtil {
-    public static T SelectWeightedObject<T>(IEnumerable<T> collection) where T : IWeightedObject {
+    public static T SelectWeightedObject<T>(IEnumerable<T> collection, Func<T, int> weightFunction) {
       if (collection == null) {
         Debug.LogWarning("SelectWeightedObject - passed in null collection!");
         return default(T);
       }
 
-      int cumulativeWeight = collection.Sum(obj => obj.Weight);
+      int cumulativeWeight = collection.Sum(obj => weightFunction.Invoke(obj));
       if (cumulativeWeight == 0) {
         Debug.LogWarning("SelectWeightedObject - cumulative weight is 0!");
         return default(T);
       }
 
-      int selectedWeight = Random.Range(0, cumulativeWeight);
+      int selectedWeight = UnityEngine.Random.Range(0, cumulativeWeight);
       foreach (T obj in collection) {
-        selectedWeight -= obj.Weight;
+        selectedWeight -= weightFunction.Invoke(obj);
         if (selectedWeight <= 0) {
           return obj;
         }
