9b794da4d9c8c08387f1f4a8a96ee821980e05c1
Darren Tsung <darren.tsung@gmail.com>
2016-03-31 09:29:44 -0700

    Add vector axis and move over RotationDirection to enums

--- Enums/RotationDirection.cs
+++ Enums/RotationDirection.cs
@@ -4,11 +4,19 @@ using System.Collections;
 
 namespace DT {
 	public enum RotationDirection {
-		CLOCKWISE = 0,
-		COUNTER_CLOCKWISE = 1
+		CLOCKWISE = 1,
+		COUNTER_CLOCKWISE = 2
 	}
 
 	public static class RotationDirectionExtensions {
+    public static float FloatValue(this RotationDirection direction) {
+      if (direction == RotationDirection.CLOCKWISE) {
+        return -1.0f;
+      } else {
+        return 1.0f;
+      }
+    }
+
 		public static RotationDirection Flipped(this RotationDirection direction) {
 			return (direction == RotationDirection.CLOCKWISE) ? RotationDirection.COUNTER_CLOCKWISE : RotationDirection.CLOCKWISE;
 		}
