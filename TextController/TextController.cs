99324f24fe17cccc66b42401c1946f2052db8736
Darren Tsung <darren.tsung@gmail.com>
2016-04-03 13:32:46 -0700

    Add text controller for outlets for every type of text

---
+++ TextController/TextController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+#if TMPRO
+using TMPro;
+#endif
+
+namespace DT {
+  [Serializable]
+  public class TextController {
+    public void SetText(string text) {
+      if (this._unityText != null) {
+        this._unityText.text = text;
+      }
+
+      if (this._tmpText != null) {
+        this._tmpText.text = text;
+      }
+    }
+
+
+    // PRAGMA MARK - Internal
+    [SerializeField]
+    private Text _unityText;
+#if TMPRO
+    [SerializeField]
+    private TMP_Text _tmpText;
+#endif
+  }
+}
