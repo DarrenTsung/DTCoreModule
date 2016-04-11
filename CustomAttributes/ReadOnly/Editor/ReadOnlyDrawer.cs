ffcf6955e33987172ef9609b02a03e24b068bd4d
Darren Tsung <darren.tsung@gmail.com>
2016-04-10 20:33:26 -0700

    Changed read only drawer to use a different way that seems like it works pretty well

--- CustomAttributes/ReadOnly/Editor/ReadOnlyDrawer.cs
+++ CustomAttributes/ReadOnly/Editor/ReadOnlyDrawer.cs
@@ -5,35 +5,9 @@ namespace DT {
 	[CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
 	public class ReadOnlyDrawer : PropertyDrawer {
 		public override void OnGUI(Rect _position, SerializedProperty _property, GUIContent _label) {
-			string valueStr;
-			switch (_property.propertyType) {
-				case SerializedPropertyType.Integer:
-					valueStr = _property.intValue.ToString();
-					break;
-				case SerializedPropertyType.Boolean:
-					valueStr = _property.boolValue.ToString();
-					break;
-				case SerializedPropertyType.Float:
-					valueStr = _property.floatValue.ToString("0.00000");
-					break;
-				case SerializedPropertyType.String:
-					valueStr = _property.stringValue;
-					break;
-				case SerializedPropertyType.Enum:
-					valueStr = _property.enumDisplayNames[_property.enumValueIndex];
-					break;
-				case SerializedPropertyType.Vector2:
-					valueStr = _property.vector2Value.ToString();
-					break;
-				case SerializedPropertyType.Vector3:
-					valueStr = _property.vector3Value.ToString();
-					break;
-				default:
-					valueStr = "(not supported)";
-					break;
-			}
-
-			EditorGUI.LabelField(_position, _label.text + ":", valueStr);
+      GUI.enabled = false;
+      EditorGUI.PropertyField(_position, _property, _label, true);
+      GUI.enabled = true;
 		}
 	}
 }
