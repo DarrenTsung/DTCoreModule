0cebe98b8ed0525a0fe1ee5b193e8c1ffea108a3
Darren Tsung <darren.tsung@gmail.com>
2016-04-12 09:56:15 -0700

    Changed toggle active to always do the smart thing.

--- SharedScripts/Misc/Editor/ToggleActiveSelectedGameObjectMenu.cs
+++ SharedScripts/Misc/Editor/ToggleActiveSelectedGameObjectMenu.cs
@@ -3,18 +3,8 @@ using UnityEditor;
 
 namespace DT {
 	public class ToggleActiveSelectedGameObjectMenu {
-		[MenuItem("DarrenTsung/Toggle Active/Toggle Active for Selected GameObject #a")]
+		[MenuItem("DarrenTsung/Toggle Active/Smart Toggle Active for Selected GameObject #a")]
 		public static void ToggleActiveSelectedGameObject() {
-      GameObject obj = Selection.activeGameObject;
-      if (obj == null) {
-        return;
-      }
-
-      obj.SetActive(!obj.activeSelf);
-		}
-
-		[MenuItem("DarrenTsung/Toggle Active/Smart Toggle Active for Selected GameObject #%a")]
-		public static void SmartToggleActiveSelectedGameObject() {
       GameObject obj = Selection.activeGameObject;
       if (obj == null) {
         return;
