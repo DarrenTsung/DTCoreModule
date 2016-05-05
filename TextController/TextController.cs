7a827fd60fc8efedce1182a9b9265e77c4fd29de
Darren Tsung <darren.tsung@gmail.com>
2016-05-05 09:28:53 -0700

    Add color to text controller

--- TextController/TextController.cs
+++ TextController/TextController.cs
@@ -24,6 +24,33 @@ namespace DT {
       }
     }
 
+    public Color Color {
+      get { return this.GetColor(); }
+      set { this.SetColor(value); }
+    }
+
+    public Color GetColor() {
+      if (this._unityText != null) {
+        return this._unityText.color;
+      }
+
+      if (this._tmpText != null) {
+        return this._tmpText.color;
+      }
+
+      return Color.white;
+    }
+
+    public void SetColor(Color color) {
+      if (this._unityText != null) {
+        this._unityText.color = color;
+      }
+
+      if (this._tmpText != null) {
+        this._tmpText.color = color;
+      }
+    }
+
 
     // PRAGMA MARK - Internal
     [SerializeField]
