63e7dc3eaa079427a46343a1f970fb7b3884c82b
Darren Tsung <darren.tsung@gmail.com>
2016-05-10 10:26:36 -0700

    Add particle system extensions for SetEmissionEnabled

---
+++ Extensions/ParticleSystemExtensions.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace DT {
+  public static class ParticleSystemExtensions {
+    public static void SetEmissionEnabled(this ParticleSystem particleSystem, bool enabled) {
+      ParticleSystem.EmissionModule em = particleSystem.emission;
+      em.enabled = enabled;
+    }
+  }
+}
