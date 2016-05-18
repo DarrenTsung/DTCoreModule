5b4c9debf96800479e54ede69afa918439775066
Darren Tsung <darren.tsung@gmail.com>
2016-05-18 10:07:30 -0700

    Allow for an empty base style for StyleWithTexture

--- Util/GUIStyleUtil.cs
+++ Util/GUIStyleUtil.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 
 namespace DT {
   public static class GUIStyleUtil {
+    private static readonly GUIStyle kEmptyGUIStyle = new GUIStyle();
     private static Dictionary<Texture2D, GUIStyle> _cachedTextureStyles = new Dictionary<Texture2D, GUIStyle>();
+
+    public static GUIStyle StyleWithTexture(Texture2D texture) {
+      return GUIStyleUtil.StyleWithTexture(kEmptyGUIStyle, texture);
+    }
 
     public static GUIStyle StyleWithTexture(GUIStyle baseStyle, Texture2D texture) {
       GUIStyle style = GUIStyleUtil._cachedTextureStyles.SafeGet(texture);
