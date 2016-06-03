db43f98f73257fded3dfc37596a1a37348dc4395
Darren Tsung <darren.tsung@gmail.com>
2016-06-03 09:46:50 -0700

    Add remove range extension for list

--- Extensions/ListExtensions.cs
+++ Extensions/ListExtensions.cs
@@ -5,16 +5,23 @@ using System.Collections.Generic;
 namespace DT {
   public static class ListExtensions {
     static Random rand = new Random();
-    
+
     public static T PickRandom<T>(this IList<T> source) {
         return source[ListExtensions.rand.Next(source.Count)];
     }
-    
+
     public static T SafeGet<T>(this IList<T> source, int index, T defaultValue = default(T)) {
       if (index >= 0 && index < source.Count) {
         return source[index];
       } else {
         return defaultValue;
+      }
+    }
+
+    public static void RemoveRange<T>(this List<T> l, IList<T> itemsToRemove) {
+      for (int i = 0; i < itemsToRemove.Count; i++) {
+        T item = itemsToRemove[i];
+        l.Remove(item);
       }
     }
   }
