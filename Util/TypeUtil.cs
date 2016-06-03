968ac73948561b5dfe50c61483accfb9d822fe05
Darren Tsung <darren.tsung@gmail.com>
2016-06-03 15:05:34 -0700

    Add GetInspectorFields for TypeUtil

--- Util/TypeUtil.cs
+++ Util/TypeUtil.cs
@@ -1,9 +1,35 @@
 using DT;
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using UnityEngine;
 
 namespace DT {
+  public static class TypeUtil {
+    // PRAGMA MARK - Static Public Interface
+    public static FieldInfo[] GetInspectorFields(Type type) {
+      if (!TypeUtil._inspectorFieldMapping.ContainsKey(type)) {
+        FieldInfo[] fields = type.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
+                                 .Where((fieldInfo) => {
+                                   if (fieldInfo.IsPublic) {
+                                     return true;
+                                   } else {
+                                     return Attribute.IsDefined(fieldInfo, typeof(SerializeField));
+                                   }
+                                 }).ToArray();
+        TypeUtil._inspectorFieldMapping[type] = fields;
+      }
+
+      return TypeUtil._inspectorFieldMapping[type];
+    }
+
+
+    // PRAGMA MARK - Static Internal
+    private static Dictionary<Type, FieldInfo[]> _inspectorFieldMapping = new Dictionary<Type, FieldInfo[]>();
+  }
+
   public static class TypeUtil<T> {
     // PRAGMA MARK - Static Public Interface
     public static Type[] ImplementationTypes {
