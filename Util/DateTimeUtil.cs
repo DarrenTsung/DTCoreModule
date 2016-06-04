8a5aef7c092090de2544cc6128081c1a5a20493f
Darren Tsung <darren.tsung@gmail.com>
2016-06-04 08:34:15 -0700

    Add SameDay util function for DateTime

---
+++ Util/DateTimeUtil.cs
@@ -0,0 +1,12 @@
+using DT;
+using System;
+﻿using System.Collections.Generic;
+﻿using UnityEngine;
+
+namespace DT {
+	public static class DateTimeUtil {
+    public static bool SameDay(DateTime a, DateTime b) {
+      return a.Date == b.Date;
+    }
+  }
+}
