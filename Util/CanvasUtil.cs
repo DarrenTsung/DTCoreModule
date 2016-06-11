8e73224c71f64510f4a69538d52db172e83d3407
Darren Tsung <darren.tsung@gmail.com>
2016-06-11 15:17:36 -0700

    Move parenting ui element to CanvasUtil

--- Util/CanvasUtil.cs
+++ Util/CanvasUtil.cs
@@ -5,6 +5,26 @@ using System.Linq;
 
 namespace DT {
 	public class CanvasUtil {
+    // PRAGMA MARK - Static Interface
+    public static GameObject ScreenSpaceMainCanvas {
+      get {
+        if (CanvasUtil._screenSpaceMainCanvas == null) {
+          CanvasUtil.FindCanvases();
+        }
+        return CanvasUtil._screenSpaceMainCanvas;
+      }
+    }
+
+    public static void ParentUIElementToCanvas(GameObject uiElement, GameObject canvas) {
+      // HACK: make this better Darren
+      uiElement.transform.SetParent(canvas.transform, false);
+      uiElement.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+      ((RectTransform)uiElement.transform).offsetMin = new Vector2(0.0f, 0.0f);
+      ((RectTransform)uiElement.transform).offsetMax = new Vector2(0.0f, 0.0f);
+    }
+
+
+    // PRAGMA MARK - Static Internal
     private static GameObject _screenSpaceMainCanvas;
 
     static CanvasUtil() {
@@ -20,15 +40,6 @@ namespace DT {
 
       CanvasUtil._screenSpaceMainCanvas = (from canvasComponent in canvasComponents
         where canvasComponent.gameObject.tag == "ScreenSpaceCanvas" select canvasComponent.gameObject).FirstOrDefault();
-    }
-
-    public static GameObject ScreenSpaceMainCanvas {
-      get {
-        if (CanvasUtil._screenSpaceMainCanvas == null) {
-          CanvasUtil.FindCanvases();
-        }
-        return CanvasUtil._screenSpaceMainCanvas;
-      }
     }
 	}
 }
