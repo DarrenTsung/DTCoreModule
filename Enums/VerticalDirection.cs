de9bf0ada3abe702cbb7339c4f402cb22ea13fec
Darren Tsung <darren.tsung@gmail.com>
2016-06-22 21:39:35 -0700

    Add VerticalDirection enum

---
+++ Enums/VerticalDirection.cs
@@ -0,0 +1,30 @@
+﻿using DT;
+using System.Collections;
+﻿using UnityEngine;
+
+namespace DT {
+	public enum VerticalDirection {
+		UP = 1,
+		DOWN = 2
+	}
+
+	public static class VerticalDirectionExtensions {
+		public static float Sign(this VerticalDirection direction) {
+			return (direction == VerticalDirection.UP) ? 1.0f : -1.0f;
+		}
+
+		public static VerticalDirection Flip(this VerticalDirection direction) {
+			return (direction == VerticalDirection.UP) ? VerticalDirection.DOWN : VerticalDirection.UP;
+		}
+
+		public static Vector2 VectorSign(this VerticalDirection direction) {
+			return (direction == VerticalDirection.UP) ? Vector2.up : -Vector2.up;
+		}
+	}
+
+  public static class VerticalDirectionUtil {
+    public static VerticalDirection RandomDirection() {
+      return (Random.value > 0.5f) ? VerticalDirection.UP : VerticalDirection.DOWN;
+    }
+  }
+}
