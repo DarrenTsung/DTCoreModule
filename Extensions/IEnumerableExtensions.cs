99a4339a437f01645175ec52c636b6a0349e9ed7
Darren Tsung <darren.tsung@gmail.com>
2016-07-04 16:00:48 -0700

    Add Min and All functions for IEnumerableExtensions

--- Extensions/IEnumerableExtensions.cs
+++ Extensions/IEnumerableExtensions.cs
@@ -4,12 +4,12 @@ using System.Collections.Generic;
 
 namespace DT {
   public static class IEnumerableExtensions {
-    public static T Max<T>(this IEnumerable<T> enumerable, Func<T, int> predicate) {
+    public static T Max<T>(this IEnumerable<T> enumerable, Func<T, int> transformation) {
       int maxSoFar = int.MinValue;
       T maxElement = default(T);
 
       foreach (T element in enumerable) {
-        int computedValue = predicate.Invoke(element);
+        int computedValue = transformation.Invoke(element);
         if (computedValue > maxSoFar) {
           maxSoFar = computedValue;
           maxElement = element;
@@ -21,6 +21,35 @@ namespace DT {
 
     public static int Max(this IEnumerable<int> enumerable) {
       return enumerable.Max(i => i);
+    }
+
+    public static T Min<T>(this IEnumerable<T> enumerable, Func<T, int> transformation) {
+      int minSoFar = int.MaxValue;
+      T minElement = default(T);
+
+      foreach (T element in enumerable) {
+        int computedValue = transformation.Invoke(element);
+        if (computedValue < minSoFar) {
+          minSoFar = computedValue;
+          minElement = element;
+        }
+      }
+
+      return minElement;
+    }
+
+    public static int Min(this IEnumerable<int> enumerable) {
+      return enumerable.Min(i => i);
+    }
+
+    public static bool All<T>(this IEnumerable<T> enumerable, Predicate<T> predicate) {
+      foreach (T element in enumerable) {
+        if (!predicate.Invoke(element)) {
+          return false;
+        }
+      }
+
+      return true;
     }
 
     public static List<T> ToList<T>(this IEnumerable<T> enumerable) {
