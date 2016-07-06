7d593b17f45044f6845cb3c4bf6c1dcade186511
Darren Tsung <darren.tsung@gmail.com>
2016-07-06 10:34:11 -0700

    Add GetValue convenience method for PropertyInfo added in later .NET version

---
+++ Extensions/PropertyInfoExtensions.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+namespace DT {
+  public static class PropertyInfoExtensions {
+    public static object GetValue(this PropertyInfo p, object obj) {
+      return p.GetValue(obj, invokeAttr: BindingFlags.Default, binder: null, index: null, culture: null);
+    }
+  }
+}
