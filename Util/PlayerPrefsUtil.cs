50bea068f189adcb7f2286bf8ff093703530a9a3
Darren Tsung <darren.tsung@gmail.com>
2016-07-17 10:20:06 -0700

    Add GetBool / SetBool to PlayerPrefUtils

--- Util/PlayerPrefsUtil.cs
+++ Util/PlayerPrefsUtil.cs
@@ -8,5 +8,13 @@ namespace DT {
       int oldValue = PlayerPrefs.GetInt(key, defaultValue);
       PlayerPrefs.SetInt(key, oldValue + 1);
     }
+
+    public static bool GetBool(string key) {
+      return PlayerPrefs.GetInt("PlayerPrefsUtilBool::" + key) > 0;
+    }
+
+    public static void SetBool(string key, bool val) {
+      PlayerPrefs.SetInt("PlayerPrefsUtilBool::" + key, val ? 1 : 0);
+    }
 	}
 }
