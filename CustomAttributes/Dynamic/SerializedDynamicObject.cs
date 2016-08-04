cc6da7ca0fcc9d59359f737e893e03f350aba1da
Darren Tsung <darren.tsung@gmail.com>
2016-08-04 09:53:26 -0700

    Expose creating a copy of the dynamic object from the serializedDynamicObject

--- CustomAttributes/Dynamic/SerializedDynamicObject.cs
+++ CustomAttributes/Dynamic/SerializedDynamicObject.cs
@@ -8,21 +8,27 @@ namespace DT {
     public string implementationTypeName;
     public string serializedDynamicObject;
 
+    public T CreateDynamicObjectFromSerialized() {
+      string[] implementationTypeNames = TypeUtil.GetImplementationTypeNames(typeof(T));
+
+      int index = Array.IndexOf(implementationTypeNames, this.implementationTypeName);
+      if (index == -1) {
+        Debug.LogError("Failed to get DynamicObject from SerializedDynamicObject during runtime because type name is not found!");
+        return null;
+      }
+
+      Type[] implementationTypes = TypeUtil.GetImplementationTypes(typeof(T));
+      Type dynamicType = implementationTypes[index];
+      T dynamicObject = (T)ScriptableObject.CreateInstance(dynamicType);
+      JsonUtility.FromJsonOverwrite(this.serializedDynamicObject, dynamicObject);
+
+      return dynamicObject;
+    }
+
     public T DynamicObject {
       get {
         if (this._cachedDynamicObject == null) {
-          string[] implementationTypeNames = TypeUtil.GetImplementationTypeNames(typeof(T));
-
-          int index = Array.IndexOf(implementationTypeNames, this.implementationTypeName);
-          if (index == -1) {
-            Debug.LogError("Failed to get DynamicObject from SerializedDynamicObject during runtime because type name is not found!");
-            return null;
-          }
-
-          Type[] implementationTypes = TypeUtil.GetImplementationTypes(typeof(T));
-          Type dynamicType = implementationTypes[index];
-          this._cachedDynamicObject = (T)ScriptableObject.CreateInstance(dynamicType);
-          JsonUtility.FromJsonOverwrite(this.serializedDynamicObject, this._cachedDynamicObject);
+          this._cachedDynamicObject = this.CreateDynamicObjectFromSerialized();
         }
 
         return this._cachedDynamicObject;
