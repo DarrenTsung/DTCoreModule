9cd9cdfbf2157aa390801d30646a4365aef37843
Darren Tsung <darren.tsung@gmail.com>
2016-08-23 07:48:59 -0700

    Add find first required object of type

--- Util/ObjectUtil.cs
+++ Util/ObjectUtil.cs
@@ -1,15 +1,28 @@
 using DT;
+using System;
 ﻿using UnityEngine;
 
 namespace DT {
 	public static class ObjectUtil {
-    public static T FindRequiredObjectOfType<T>() where T : Object {
+    public static T FindRequiredObjectOfType<T>() where T : UnityEngine.Object {
       T obj;
-      obj = (T)Object.FindObjectOfType(typeof(T));
+      obj = (T)UnityEngine.Object.FindObjectOfType(typeof(T));
       if (obj == null) {
         Debug.LogError("Failed to find required object of type: " + typeof(T).Name + "!");
       }
       return obj;
+    }
+
+    public static T FindFirstRequiredObjectOfType<T>(Predicate<T> predicate) where T : UnityEngine.Object {
+      T[] objects = (T[])UnityEngine.Object.FindObjectsOfType(typeof(T));
+      foreach (T obj in objects) {
+        if (predicate.Invoke(obj)) {
+          return obj;
+        }
+      }
+
+      Debug.LogError("Failed to find required object of type: " + typeof(T).Name + "!");
+      return default(T);
     }
 	}
 }
