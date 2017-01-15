88666e042eae11c401ef0be2781ebf7e0716bd61
Darren Tsung <darren@tangibleplay.com>
2017-01-15 00:07:59 -0800

    Add inverse scale as method in Vector3Util

--- Util/Vector3Util.cs
+++ Util/Vector3Util.cs
@@ -2,6 +2,13 @@
 
 namespace DT {
 	public static class Vector3Util {
+    public static Vector3 InverseScale(Vector3 a, Vector3 b) {
+        a.x = (b.x == 0.0f) ? 0.0f : a.x / b.x;
+        a.y = (b.y == 0.0f) ? 0.0f : a.y / b.y;
+        a.z = (b.z == 0.0f) ? 0.0f : a.z / b.z;
+        return a;
+    }
+
     public static Vector3 RandomBetweenMinMaxVectors(Vector3 min, Vector3 max) {
       if (min.x > max.x || min.y > max.y || min.z > max.z) {
         Debug.LogError("RandomBetweenMinMaxVectors - min (" + min + ") greater than max (" + max + ")!");
