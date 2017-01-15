41f8a7faa953c5f9f624ac802c803bd88c6ca40f
Darren Tsung <darren@tangibleplay.com>
2017-01-15 00:08:20 -0800

    Add ScaledSpriteRenderer to make sure sprites are right size

---
+++ SharedScripts/Misc/ScaledSpriteRenderer.cs
@@ -0,0 +1,57 @@
+using DT;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace DT.Game {
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class ScaledSpriteRenderer : MonoBehaviour {
+        // PRAGMA MARK - Internal
+        [SerializeField, ReadOnly] private Vector3 _size = Vector3.one;
+
+        private Sprite _cachedSprite = null;
+        private SpriteRenderer _spriteRenderer;
+
+        private SpriteRenderer _SpriteRenderer {
+            get {
+                if (_spriteRenderer == null) {
+                    _spriteRenderer = GetComponent<SpriteRenderer>();
+                }
+                return _spriteRenderer;
+            }
+        }
+
+        void OnEnable() {
+            MonoBehaviourHelper.OnUpdate += HandleUpdate;
+        }
+
+        void OnDisable() {
+            MonoBehaviourHelper.OnUpdate -= HandleUpdate;
+        }
+
+        void OnDrawGizmos() {
+            Gizmos.DrawWireCube(transform.position, _size);
+        }
+
+        private void HandleUpdate() {
+            if (_cachedSprite != _SpriteRenderer.sprite) {
+                ResetSize();
+                _cachedSprite = _SpriteRenderer.sprite;
+            }
+        }
+
+        [MakeButton]
+        private void ResetSize() {
+            Vector3 baseSize = Vector3Util.InverseScale(_SpriteRenderer.bounds.size, transform.lossyScale);
+            Vector3 scale = Vector3Util.InverseScale(_size, baseSize);
+            transform.localScale = scale;
+        }
+
+        [MakeButton]
+        private void SetSize() {
+            _size = _SpriteRenderer.bounds.size;
+        }
+    }
+}
