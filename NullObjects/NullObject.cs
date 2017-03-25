398b24b229def6e5ac3040cd06b2f2ff5f0c786a
Darren Tsung <darrentsung@Darrens-MacBook-Pro.local>
2017-03-25 15:50:15 -0700

    Remove NullObject

--- NullObjects/NullObject.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-
-namespace DT {
-  public class NullObject<TSubclassNullObject> where TSubclassNullObject : new() {
-    public static TSubclassNullObject Instance {
-      get { return SingletonUtil<TSubclassNullObject>.Instance; }
-    }
-  }
-}
