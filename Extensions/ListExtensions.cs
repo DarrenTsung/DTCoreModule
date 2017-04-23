ef960c39bc0878eebcbd4de8eef4f14a550b21d7
Darren Tsung <darrentsung@Darrens-MacBook-Pro.local>
2017-04-23 09:13:28 -0700

    Add shuffle to ListExtensions

--- Extensions/ListExtensions.cs
+++ Extensions/ListExtensions.cs
@@ -21,6 +21,17 @@ namespace DT {
 			return item;
 		}
 
+		public static void Shuffle<T>(this IList<T> list) {
+			var count = list.Count;
+			var last = count - 1;
+			for (var i = 0; i < last; ++i) {
+				var r = UnityEngine.Random.Range(i, count);
+				var tmp = list[i];
+				list[i] = list[r];
+				list[r] = tmp;
+			}
+		}
+
 		public static IEnumerable<T> Repeat<T>(this IList<T> list) {
 			int index = 0;
 			while (true) {
