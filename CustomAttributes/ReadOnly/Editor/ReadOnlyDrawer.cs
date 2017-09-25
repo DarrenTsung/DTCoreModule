5b7b7976e24619c26895fb973c70fd1e9b014435
Darren Tsung <darren.tsung@gmail.com>
2017-09-25 09:40:52 -0700

    Fix readonly property drawer not correctly drawing when child properties open

--- CustomAttributes/ReadOnly/Editor/ReadOnlyDrawer.cs
+++ CustomAttributes/ReadOnly/Editor/ReadOnlyDrawer.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 namespace DT {
 	[CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
 	public class ReadOnlyDrawer : PropertyDrawer {
+		public override float GetPropertyHeight(SerializedProperty property, GUIContent label) {
+			return EditorGUI.GetPropertyHeight(property, label, true);
+		}
+
 		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
 			GUI.enabled = false;
 			EditorGUI.PropertyField(position, property, label, true);
