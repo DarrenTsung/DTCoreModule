32f68ee480127b906f736f2a71328cf420e06466
Darren Tsung <darren.tsung@gmail.com>
2017-11-01 11:22:09 -0700

    Cleanup CoroutineWrapper to find bug where coroutine wrapped was not stopped - was not in this code. Kept the cleaned up version anyways.

--- CoroutineWrapper/CoroutineWrapper.cs
+++ CoroutineWrapper/CoroutineWrapper.cs
@@ -5,36 +5,32 @@ using UnityEngine;
 namespace DT {
 	public partial class CoroutineWrapper {
 		// PRAGMA MARK - Static
-		public static CoroutineWrapper StartCoroutine(IEnumerator coroutine, Action finishedCallback) {
-			return new CoroutineWrapper(coroutine, finishedCallback);
-		}
-
-		public static CoroutineWrapper StartCoroutine(IEnumerator coroutine, Action<bool> finishedCallback = null) {
-			return new CoroutineWrapper(coroutine, finishedCallback);
+		public static CoroutineWrapper StartCoroutine(IEnumerator coroutine) {
+			return new CoroutineWrapper(coroutine);
 		}
 
 		public static CoroutineWrapper DoAfterRealtimeDelay(float delay, Action finishedCallback) {
-			return StartCoroutine(RealtimeDelayCoroutine(delay), finishedCallback);
+			return StartCoroutine(RealtimeDelayCoroutine(delay, finishedCallback));
 		}
 
 		public static CoroutineWrapper DoAfterDelay(float delay, Action finishedCallback) {
-			return StartCoroutine(DelayCoroutine(delay), finishedCallback);
+			return StartCoroutine(DelayCoroutine(delay, finishedCallback));
 		}
 
 		public static CoroutineWrapper DoEveryFrameForDelay(float delay, Action perFrameCallback, Action finishedCallback) {
-			return StartCoroutine(DelayFrameCoroutine(delay, perFrameCallback), finishedCallback);
+			return StartCoroutine(DelayFrameCoroutine(delay, perFrameCallback, finishedCallback));
 		}
 
 		public static CoroutineWrapper DoAfterFrame(Action finishedCallback) {
-			return StartCoroutine(WaitOneFrameCoroutine(), finishedCallback);
+			return StartCoroutine(WaitOneFrameCoroutine(finishedCallback));
 		}
 
 		public static CoroutineWrapper DoAtEndOfFrame(Action finishedCallback) {
-			return StartCoroutine(EndOfFrameCoroutine(), finishedCallback);
+			return StartCoroutine(EndOfFrameCoroutine(finishedCallback));
 		}
 
 		public static CoroutineWrapper DoLerpFor(float duration, Action<float> lerpCallback, Action finishedCallback = null) {
-			return StartCoroutine(DoLerpCoroutine(duration, lerpCallback), finishedCallback);
+			return StartCoroutine(DoLerpCoroutine(duration, lerpCallback, finishedCallback));
 		}
 
 
@@ -47,90 +43,60 @@ namespace DT {
 			GameObject.DontDestroyOnLoad(coroutineRunnerObject);
 		}
 
-		private static IEnumerator DoLerpCoroutine(float duration, Action<float> lerpCallback) {
+		private static IEnumerator DoLerpCoroutine(float duration, Action<float> lerpCallback, Action finishedCallback) {
 			for (float time = 0.0f; time <= duration; time += Time.deltaTime) {
 				lerpCallback.Invoke(time / duration);
 				yield return null;
 			}
 
 			lerpCallback.Invoke(1.0f);
+			if (finishedCallback != null) {
+				finishedCallback.Invoke();
+			}
 		}
 
-		private static IEnumerator RealtimeDelayCoroutine(float delay) {
+		private static IEnumerator RealtimeDelayCoroutine(float delay, Action finishedCallback) {
 			yield return new WaitForSecondsRealtime(delay);
+			finishedCallback.Invoke();
 		}
 
-		private static IEnumerator DelayCoroutine(float delay) {
+		private static IEnumerator DelayCoroutine(float delay, Action finishedCallback) {
 			yield return new WaitForSeconds(delay);
+			finishedCallback.Invoke();
 		}
 
 		private static readonly WaitForEndOfFrame kWaitForEndOfFrame = new WaitForEndOfFrame();
-		private static IEnumerator EndOfFrameCoroutine() {
+		private static IEnumerator EndOfFrameCoroutine(Action finishedCallback) {
 			yield return kWaitForEndOfFrame;
+			finishedCallback.Invoke();
 		}
 
-		private static IEnumerator WaitOneFrameCoroutine() {
+		private static IEnumerator WaitOneFrameCoroutine(Action finishedCallback) {
 			yield return null;
+			finishedCallback.Invoke();
 		}
 
-		private static IEnumerator DelayFrameCoroutine(float delay, Action perFrameCallback) {
+		private static IEnumerator DelayFrameCoroutine(float delay, Action perFrameCallback, Action finishedCallback) {
 			for (float time = 0.0f; time <= delay; time += Time.deltaTime) {
 				perFrameCallback.Invoke();
 				yield return null;
 			}
+
+			finishedCallback.Invoke();
 		}
 
 
 		// PRAGMA MARK - Public Interface
-		public void Stop() {
-			running_ = false;
-			manuallyStopped_ = true;
-		}
-
 		public void Cancel() {
-			running_ = false;
-			cancelled_ = true;
+			coroutineRunner_.StopCoroutine(coroutine_);
 		}
 
 
 		// PRAGMA MARK - Internal
-		private CoroutineWrapper(IEnumerator coroutine, Action finishedCallback) : this(coroutine) {
-			finishedNoArgsCallback_ = finishedCallback;
-		}
-
-		private CoroutineWrapper(IEnumerator coroutine, Action<bool> finishedCallback) : this(coroutine) {
-			finishedArgsCallback_ = finishedCallback;
-		}
-
 		private CoroutineWrapper(IEnumerator coroutine) {
-			coroutine_ = coroutine;
-			coroutineRunner_.StartCoroutine(Coroutine());
+			coroutine_ = coroutineRunner_.StartCoroutine(coroutine);
 		}
 
-		private Action finishedNoArgsCallback_;
-		private Action<bool> finishedArgsCallback_;
-		private IEnumerator coroutine_;
-
-		private bool manuallyStopped_ = false;
-		private bool running_ = true;
-		private bool cancelled_ = false;
-
-		private IEnumerator Coroutine() {
-			while (running_) {
-				if (coroutine_ != null && coroutine_.MoveNext()) {
-					yield return coroutine_.Current;
-				} else {
-					running_ = false;
-				}
-			}
-
-			if (!cancelled_ && finishedNoArgsCallback_ != null) {
-				finishedNoArgsCallback_.Invoke();
-			}
-
-			if (!cancelled_ && finishedArgsCallback_ != null) {
-				finishedArgsCallback_(manuallyStopped_);
-			}
-		}
+		private Coroutine coroutine_;
 	}
 }
